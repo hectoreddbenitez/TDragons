40d4f1d8dd48289b802005627c403a3693f989e7
hector <hectoreddbenitez@gmail.com>
2022-04-14 19:53:07 -0300

    Add: New Character class, unfinished

---
+++ src/Character.ts
@@ -0,0 +1,65 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._race = new Elf(name, 10);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._strength = 10;
+    this._defense = 10;
+    this._dexterity = this._race.dexterity;
+    this._energy = { type_: this._archetype.energyType, amount: 8 };
+  }
+
+  get race(): Race {
+    return this._race;
+  }
+
+  get archetype(): Archetype {
+    return this._archetype;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  get defense(): number {
+    return this._defense;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  get energy(): Energy {
+    return { ...this._energy };
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense;
+    if (damage > 0) {
+      if (this._lifePoints < 1) {
+        return -1;
+      }
+      return this._lifePoints - damage;
+    }
+    return this._lifePoints;
+  }
+}
