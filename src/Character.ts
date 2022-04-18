7c367c51994fa3217b1f5574131a0748de5857b7
hector <hectoreddbenitez@gmail.com>
2022-04-18 11:16:31 -0300

    Add: fully implemented Character class

--- src/Character.ts
+++ src/Character.ts
@@ -2,6 +2,7 @@ import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
 import Fighter from './Fighter';
 import Race, { Elf } from './Races';
+import getRandomInt from './utils';
 
 export default class Character implements Fighter {
   private _race: Race;
@@ -52,14 +53,36 @@ export default class Character implements Fighter {
     return { ...this._energy };
   }
 
+  // tive que consultar PR de dois colegas, para entender a lógica do código. 
   receiveDamage(attackPoints: number): number {
     const damage = attackPoints - this._defense;
     if (damage > 0) {
-      if (this._lifePoints < 1) {
-        return -1;
-      }
-      return this._lifePoints - damage;
+      this._lifePoints -= damage;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
     }
     return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  special(enemy: Fighter): Fighter {
+    this._maxLifePoints += 20;
+    return enemy;
   }
 }
